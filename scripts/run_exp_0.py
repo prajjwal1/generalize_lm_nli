d8a61a6ac03e1659a3f1dbbd80162354ea2004e6
Sasha <alexander.drozd@gmail.com>
2020-05-28 23:38:27 +0900

    copy dev

--- scripts/run_exp_0.py
+++ scripts/run_exp_0.py
@@ -3,25 +3,27 @@ from pathlib import Path
 
 path_data = Path("/mnt/storage/Data/NLP/datasets/importance_sampling/")
 # TODO: iterate all subfolders
-seed = 42
-cnt_samples = 256
-subsample_type = "subsample_rand"
 model_name = "albert-base-v2"
-command = ["python3", "run_glue.py",
-           "--model_name_or_path", model_name,
-           "--task_name", "MNLI",
-           "--do_train",
-           "--do_eval",
-           "--data_dir", path_data / subsample_type / f"seed_{seed}" / str(cnt_samples),
-           "--max_seq_length", "128",
-           "--per_gpu_train_batch_size", "256",
-           "--learning_rate", "2e-5",
-           "--num_train_epochs", "3.0",
-           "--output_dir",
-           path_data / "results" / model_name / subsample_type / f"seed_{seed}" / str(cnt_samples),
-           "--fp16",
-           "--data_pct 0.02"]
+subsample_type = "subsample_rand"
+for dir_seed in (path_data / subsample_type).iterdir():
+    seed = int(str(dir_seed).split("_")[-1])
+    print(seed)
+    for cnt_samples in dir_seed.iterdir():
+        command = ["python3", "run_glue.py",
+                   "--model_name_or_path", model_name,
+                   "--task_name", "MNLI",
+                   "--do_train",
+                   "--do_eval",
+                   "--data_dir", cnt_samples,
+                   "--max_seq_length", "128",
+                   "--per_gpu_train_batch_size", "256",
+                   "--learning_rate", "2e-5",
+                   "--num_train_epochs", "3.0",
+                   "--output_dir",
+                   path_data / "results" / model_name / subsample_type / f"seed_{seed}" / cnt_samples.name,
+                   "--fp16",
+                   "--cbt_samples", cnt_samples.name]
 
 
-proc = subprocess.Popen(command, shell=False)
-proc.communicate()
+        proc = subprocess.Popen(command, shell=False)
+        proc.communicate()
--- scripts/subsampl_rand.py
+++ scripts/subsampl_rand.py
@@ -1,4 +1,5 @@
 import pandas
+import shutil
 from pathlib import Path
 
 # TODO: set data path specific to the node user
@@ -12,10 +13,10 @@ df = pandas.read_csv(path_data / "reference/MNLI/train.tsv",
 # TODO: iterate over seeds and sampe sizes
 seed = 42
 cnt_samples = 256
-# TODO: maybe N sample from each class??
+# TODO: maybe N sample from each class?
 subsample = df.sample(n=cnt_samples, random_state=seed)
-print(subsample)
 path_out = path_data / "subsample_rand" / f"seed_{seed}" / f"{cnt_samples}"
 path_out.mkdir(parents=True, exist_ok=True)
 df.to_csv(path_out / "train.tsv", sep="\t")
-# TODO: copy dev set
+shutil.copy(path_data / "reference/MNLI/dev_matched.tsv", path_out / "dev_matched.tsv")
+shutil.copy(path_data / "reference/MNLI/dev_mismatched.tsv", path_out / "dev_mismatched.tsv")
