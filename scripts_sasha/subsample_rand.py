85cf72598508771a5ee6f528216953e9d19cd5c2
Sasha <alexander.drozd@gmail.com>
2020-05-29 08:10:06 +0900

    iterate over seeds and sizes

--- scripts_sasha/subsample_rand.py
+++ scripts_sasha/subsample_rand.py
@@ -10,17 +10,22 @@ df = pandas.read_csv(path_data / "reference/MNLI/train.tsv",
                      header=0,
                      quoting=3)
 
-# TODO: iterate over seeds and sampe sizes
-seed = 42
-cnt_samples_per_class = 1
-cnt_samples = cnt_samples_per_class * 3
-# random sampling
-# subsample = df.sample(n=cnt_samples, random_state=seed)
-# sample from each label equally
-subsample = df.sample(frac=1, random_state=seed).groupby('gold_label').head(cnt_samples_per_class)
-path_out = path_data / "subsample_rand" / f"seed_{seed}" / f"{cnt_samples}"
-path_out.mkdir(parents=True, exist_ok=True)
-print("saving to", path_out)
-subsample.to_csv(path_out / "train.tsv", sep="\t")
-shutil.copy(path_data / "reference/MNLI/dev_matched.tsv", path_out / "dev_matched.tsv")
-shutil.copy(path_data / "reference/MNLI/dev_mismatched.tsv", path_out / "dev_mismatched.tsv")
+print("loaded", df.shape)
+for seed in range(42, 52):
+    cnt_samples_per_class = 1
+    while True:
+        cnt_samples = cnt_samples_per_class * 3
+        if cnt_samples > len(df):
+            cnt_samples = len(df)
+        shuffled = df.sample(frac=1, random_state=seed)
+        subsample = shuffled.groupby('gold_label').head(cnt_samples_per_class)
+        path_out = path_data / "subsample_rand" / f"seed_{seed}" / f"{cnt_samples}"
+        path_out.mkdir(parents=True, exist_ok=True)
+        print("saving to", path_out)
+        subsample.to_csv(path_out / "train.tsv", sep="\t")
+        shutil.copy(path_data / "reference/MNLI/dev_matched.tsv", path_out / "dev_matched.tsv")
+        shutil.copy(path_data / "reference/MNLI/dev_mismatched.tsv",
+                    path_out / "dev_mismatched.tsv")
+        if cnt_samples == len(df):
+            break
+        cnt_samples_per_class *= 2
