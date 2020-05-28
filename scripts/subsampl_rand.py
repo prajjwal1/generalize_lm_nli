ac7885b8b3d77e98497a6092125b4b19428babae
Sasha <alexander.drozd@gmail.com>
2020-05-28 22:59:39 +0900

    save to dir

--- scripts/subsampl_rand.py
+++ scripts/subsampl_rand.py
@@ -5,11 +5,16 @@ from pathlib import Path
 # read from ~/.config/something or at least hard-code by host-name
 path_data = Path("/mnt/storage/Data/NLP/datasets/importance_sampling/")
 df = pandas.read_csv(path_data / "reference/MNLI/train.tsv",
-                     delimiter="\t", header=0, quoting=3)
+                     sep="\t",
+                     header=0,
+                     quoting=3)
 
 # TODO: iterate over seeds and sampe sizes
 seed = 42
 cnt_samples = 256
 subsample = df.sample(n=cnt_samples, random_state=seed)
 print(subsample)
-# TODO: save to new folder named with seeds and size
+path_out = path_data / "subsample_rand" / f"seed_{seed}" / f"{cnt_samples}" / "MNLI/"
+path_out.mkdir(parents=True, exist_ok=True)
+df.to_csv(path_out / "train.tsv", sep="\t")
+# TODO: copy dev set
