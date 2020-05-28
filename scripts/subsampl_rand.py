2496ac0b36d8ff8a68c17b6500438a3da3eaea6c
Sasha <alexander.drozd@gmail.com>
2020-05-28 22:55:04 +0900

    take a sample

--- scripts/subsampl_rand.py
+++ scripts/subsampl_rand.py
@@ -5,4 +5,11 @@ from pathlib import Path
 # read from ~/.config/something or at least hard-code by host-name
 path_data = Path("/mnt/storage/Data/NLP/datasets/importance_sampling/")
 df = pandas.read_csv(path_data / "reference/MNLI/train.tsv",
-    delimiter="\t", header=0, quoting=3)
+                     delimiter="\t", header=0, quoting=3)
+
+# TODO: iterate over seeds and sampe sizes
+seed = 42
+cnt_samples = 256
+subsample = df.sample(n=cnt_samples, random_state=seed)
+print(subsample)
+# TODO: save to new folder named with seeds and size
