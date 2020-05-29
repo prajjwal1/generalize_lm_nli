8daa3826d5e50c37599af27634e44383855fcce7
prajjwal1 <prajjwalin@protonmail.com>
2020-05-29 19:22:54 +0900

    fixed bug in dataloader iteration

--- get_embeddings.py
+++ get_embeddings.py
@@ -6,8 +6,12 @@ from torch.utils.data.dataloader import DataLoader
 from tqdm.auto import tqdm
 from transformers import AutoConfig, AutoModel, AutoTokenizer, GlueDataset
 from transformers import GlueDataTrainingArguments as DataTrainingArguments
-from transformers import (HfArgumentParser, TrainingArguments,
-                          glue_output_modes, glue_tasks_num_labels)
+from transformers import (
+    HfArgumentParser,
+    TrainingArguments,
+    glue_output_modes,
+    glue_tasks_num_labels,
+)
 from transformers.data.data_collator import DefaultDataCollator
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -20,16 +24,32 @@ class ModelArguments:
     """
 
     model_name_or_path: str = field(
-        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+        metadata={
+            "help": (
+                "Path to pretrained model or model identifier from"
+                " huggingface.co/models"
+            )
+        }
     )
     config_name: Optional[str] = field(
-        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+        default=None,
+        metadata={
+            "help": "Pretrained config name or path if not the same as model_name"
+        },
     )
     tokenizer_name: Optional[str] = field(
-        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+        default=None,
+        metadata={
+            "help": "Pretrained tokenizer name or path if not the same as model_name"
+        },
     )
     cache_dir: Optional[str] = field(
-        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+        default=None,
+        metadata={
+            "help": (
+                "Where do you want to store the pretrained models downloaded from s3"
+            )
+        },
     )
 
 
@@ -46,7 +66,9 @@ config = AutoConfig.from_pretrained(
     cache_dir=model_args.cache_dir,
 )
 tokenizer = AutoTokenizer.from_pretrained(
-    model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+    model_args.tokenizer_name
+    if model_args.tokenizer_name
+    else model_args.model_name_or_path,
     cache_dir=model_args.cache_dir,
 )
 model = (
@@ -63,17 +85,27 @@ model = (
 train_dataset = GlueDataset(data_args, tokenizer=tokenizer)
 eval_dataset = GlueDataset(data_args, tokenizer=tokenizer, evaluate=True)
 data_collator = DefaultDataCollator()
-dataloader = DataLoader(train_dataset, batch_size=training_args.per_gpu_train_batch_size, shuffle=False, collate_fn=data_collator.collate_batch,)
+dataloader = DataLoader(
+    train_dataset,
+    batch_size=training_args.per_gpu_train_batch_size,
+    shuffle=False,
+    collate_fn=data_collator.collate_batch,
+)
 print("Extraction of Embeddings in progress")
 cls_embeddings = []
-for i in tqdm(dataloader):
-    inputs = next(iter(dataloader))
-    inputs.pop("labels")
-    for k, v in inputs.items():
-        inputs[k] = v.to(device)
-    output = model(**inputs)
-    cls_embeddings.append(output[0][:, 0, :].cpu().detach().numpy())  # CLS Token representation
-    del inputs, output
+for input_seq in tqdm(dataloader):
+    # inputs = next(iter(dataloader))
+    input_seq.pop("labels")
+    for k, v in input_seq.items():
+        input_seq[k] = v.to(device)
+    output = model(**input_seq)
+    cls_embeddings.append(
+        output[0][:, 0, :].cpu().detach().numpy()
+    )  # CLS Token representation
+    del input_seq, output
 
 print("Storing embeddings at ", training_args.output_dir)
-torch.save(cls_embeddings, training_args.output_dir + "cls_embeddings_" + data_args.task_name + ".pth")
+torch.save(
+    cls_embeddings,
+    training_args.output_dir + "cls_embeddings_" + data_args.task_name + ".pth",
+)
--- train_clustering.py
+++ train_clustering.py
@@ -8,12 +8,25 @@ from typing import Dict, Optional
 import numpy as np
 import torch
 from sklearn.cluster import DBSCAN
-from transformers import (AutoConfig, AutoModelForSequenceClassification,
-                          AutoTokenizer, EvalPrediction, GlueDataset)
+from transformers import (
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    EvalPrediction,
+    GlueDataset,
+)
 from transformers import GlueDataTrainingArguments as DataTrainingArguments
-from transformers import (HfArgumentParser, Trainer, TrainingArguments,
-                          glue_compute_metrics, glue_output_modes,
-                          glue_tasks_num_labels, set_seed)
+from transformers import (
+    HfArgumentParser,
+    Trainer,
+    TrainingArguments,
+    glue_compute_metrics,
+    glue_output_modes,
+    glue_tasks_num_labels,
+    set_seed,
+)
+
+from core import Clustering_Processor
 
 logger = logging.getLogger(__name__)
 
@@ -22,12 +35,33 @@ logger = logging.getLogger(__name__)
 class Clustering_Arguments:
     eps: float = field(metadata={"help": "Threshold value to form clusters"})
     min_samples: int = field(metadata={"help": "Minimum samples for clustering"})
-    embedding_path: str = field(metadata={"help": "Path from where embeddings will be loaded"})
-    data_pct: float = field(metadata={"help": "specifies how much data will be used"})
-    cluster_output_path: str = field(default=None, metadata={"help": "Path where embedding will be stored"})
-    cluster_labels_path: Optional[str] = field(
-        default=None, metadata={"help": "Path from there clustering labels will be loaded"}
+    embedding_path: str = field(
+        metadata={"help": "Path from where embeddings will be loaded"}
     )
+    data_pct: Optional[float] = field(
+        default=None, metadata={"help": "specifies how much data will be used"}
+    )
+    num_clusters: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": (
+                "specifies the number of clusters that will be used. If this is"
+                " enabled, `data_pct` should be set to None"
+            )
+        },
+    )
+    cluster_output_path: str = field(
+        default=None, metadata={"help": "Path where embedding will be stored"}
+    )
+    cluster_input_path: Optional[str] = field(
+        default=None,
+        metadata={"help": "Path from there clustering labels will be loaded"},
+    )
+    cluster_n_jobs: Optional[int] = field(
+        default=-1,
+        metdadata={"help": "Number of parallel processes to run for clustering"},
+    )
+    cluster_only: Optional[bool] = field(metadata={"help": "Run only clustering"})
 
 
 @dataclass
@@ -37,27 +71,52 @@ class ModelArguments:
     """
 
     model_name_or_path: str = field(
-        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+        metadata={
+            "help": (
+                "Path to pretrained model or model identifier from"
+                " huggingface.co/models"
+            )
+        }
     )
     config_name: Optional[str] = field(
-        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+        default=None,
+        metadata={
+            "help": "Pretrained config name or path if not the same as model_name"
+        },
     )
     tokenizer_name: Optional[str] = field(
-        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+        default=None,
+        metadata={
+            "help": "Pretrained tokenizer name or path if not the same as model_name"
+        },
     )
     cache_dir: Optional[str] = field(
-        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+        default=None,
+        metadata={
+            "help": (
+                "Where do you want to store the pretrained models downloaded from s3"
+            )
+        },
     )
 
 
 def main():
 
-    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments, Clustering_Arguments))
+    parser = HfArgumentParser(
+        (ModelArguments, DataTrainingArguments, TrainingArguments, Clustering_Arguments)
+    )
 
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
-        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+        model_args, data_args, training_args = parser.parse_json_file(
+            json_file=os.path.abspath(sys.argv[1])
+        )
     else:
-        model_args, data_args, training_args, clustering_args = parser.parse_args_into_dataclasses()
+        (
+            model_args,
+            data_args,
+            training_args,
+            clustering_args,
+        ) = parser.parse_args_into_dataclasses()
 
     if (
         os.path.exists(training_args.output_dir)
@@ -66,7 +125,8 @@ def main():
         and not training_args.overwrite_output_dir
     ):
         raise ValueError(
-            f"Output directory ({training_args.output_dir}) already exists and is not empty. Use --overwrite_output_dir to overcome."
+            f"Output directory ({training_args.output_dir}) already exists and is not"
+            " empty. Use --overwrite_output_dir to overcome."
         )
 
     # Setup logging
@@ -76,7 +136,8 @@ def main():
         level=logging.INFO if training_args.local_rank in [-1, 0] else logging.WARN,
     )
     logger.warning(
-        "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
+        "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits"
+        " training: %s",
         training_args.local_rank,
         training_args.device,
         training_args.n_gpu,
@@ -97,26 +158,32 @@ def main():
     logger.info("Loading embeddings")
     try:
         os.path.isfile(clustering_args.embedding_path)
-        if clustering_args.cluster_labels_path:
-            os.path.isfile(clustering_args.cluster_labels_path)
-        # else:
-        #    raise ValueError(f"Cluster labels not found at ({clustering_args.cluster_labels_path}")
+        if clustering_args.cluster_input_path:
+            os.path.isfile(clustering_args.cluster_input_path)
+        else:
+            raise ValueError(
+                f"Cluster labels not found at ({clustering_args.cluster_labels_path}"
+            )
     except FileNotFoundError:
-        raise ValueError(f"Embeddings not found at ({clustering.embedding_path})")
+        raise ValueError(f"Embeddings not found at ({clustering_args.embedding_path})")
 
     embeddings = torch.load(clustering_args.embedding_path)
     embeddings = np.concatenate(embeddings)
-    logging.info("*** Loaded ", len(embeddings), " samples ***")
-    # Load pretrained model and tokenizer
+    logging.info("*** Loaded %s samples ***", len(embeddings))
 
+    # Load pretrained model and tokenizer
     config = AutoConfig.from_pretrained(
-        model_args.config_name if model_args.config_name else model_args.model_name_or_path,
+        model_args.config_name
+        if model_args.config_name
+        else model_args.model_name_or_path,
         num_labels=num_labels,
         finetuning_task=data_args.task_name,
         cache_dir=model_args.cache_dir,
     )
     tokenizer = AutoTokenizer.from_pretrained(
-        model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+        model_args.tokenizer_name
+        if model_args.tokenizer_name
+        else model_args.model_name_or_path,
         cache_dir=model_args.cache_dir,
     )
     model = AutoModelForSequenceClassification.from_pretrained(
@@ -126,42 +193,52 @@ def main():
         cache_dir=model_args.cache_dir,
     )
 
-    logging.info("Forming clusters")
-    if clustering_args.cluster_labels_path is None:
-        clustering = DBSCAN(eps=clustering_args.eps, min_samples=clustering_args.min_samples).fit(embeddings)
-        with open(clustering_args.cluster_output_path + "/" + "cluster_labels.npy", "wb") as f:
-            np.save(f, clustering.labels_)
-            logging.info("*** INFO: Clustering labels saved ***")
+    if clustering_args.cluster_output_path and not clustering_args.cluster_input_path:
+        logging.info("Forming clusters")
+        clustering = DBSCAN(
+            eps=clustering_args.eps,
+            min_samples=clustering_args.min_samples,
+            n_jobs=clustering.cluster_n_jobs,
+        ).fit(embeddings)
+
+        torch.save(vars(clustering), clustering_args.cluster_output_path)
+
+        logging.info(
+            "*** INFO: Clustering labels saved at"
+            " ({clustering_args.cluster_output_path})***"
+        )
+        if clustering_args.cluster_only:
+            sys.exit(0)
     else:
-        cluster_labels = np.load(clustering_args.cluster_labels_path)
+        clustering = torch.load(clustering_args.cluster_input_path)
         logging.info("INFO: Clustering labels loaded")
 
-        class cluster_labels_patch:
-            def __init__(self, cluster_labels):
-                self.labels_ = cluster_labels
-
-        clustering = cluster_labels_patch(cluster_labels)
-
-    def get_cluster_indices(cluster_num, labels_array):
-        return np.where(labels_array == cluster_num)[0]
-
-    def get_concat_cluster_indices(labels, data_pct, original_len):
-        current_len, cluster_indices = 0, []
-        for i in set(labels):
-            curr_cluster_indices = get_cluster_indices(i, labels)
-            current_len += len(curr_cluster_indices)
-            if current_len < int(original_len * data_pct):
-                cluster_indices.extend(curr_cluster_indices)
-            else:
-                return cluster_indices
-
     if training_args.do_train:
+        if clustering_args.data_pct and clustering_args.num_clusters:
+            raise ValueError("You can either specify `data_pct` or `num_clusters`")
         train_dataset = GlueDataset(data_args, tokenizer)
-        cluster_indices = get_concat_cluster_indices(clustering.labels_, clustering_args.data_pct, len(train_dataset))
+
+        clustering_proc = Clustering_Processor(clustering)
+        if clustering_args.data_pct:
+            cluster_indices = clustering_proc.get_cluster_indices_by_pct(
+                clustering_args.data_pct, len(train_dataset)
+            )
+        if clustering_args.num_clusters:
+            cluster_indices = clustering_proc.get_cluster_indices_by_num(
+                clustering_args.num_clusters
+            )
+
         train_dataset = torch.utils.data.Subset(train_dataset, cluster_indices)
-        if len(train_dataset) < 100:
-            sys.exit(0)
-    eval_dataset = GlueDataset(data_args, tokenizer=tokenizer, evaluate=True) if training_args.do_eval else None
+
+        # SANITY CHECK
+        if len(train_dataset) < 10:
+            raise ValueError("Length of Dataset is less than 10")
+
+    eval_dataset = (
+        GlueDataset(data_args, tokenizer=tokenizer, evaluate=True)
+        if training_args.do_eval
+        else None
+    )
 
     def compute_metrics(p: EvalPrediction) -> Dict:
         if output_mode == "classification":
@@ -181,7 +258,9 @@ def main():
     # Training
     if training_args.do_train:
         trainer.train(
-            model_path=model_args.model_name_or_path if os.path.isdir(model_args.model_name_or_path) else None
+            model_path=model_args.model_name_or_path
+            if os.path.isdir(model_args.model_name_or_path)
+            else None
         )
         trainer.save_model()
     # Evaluation
@@ -193,16 +272,21 @@ def main():
         eval_datasets = [eval_dataset]
         if data_args.task_name == "mnli":
             mnli_mm_data_args = dataclasses.replace(data_args, task_name="mnli-mm")
-            eval_datasets.append(GlueDataset(mnli_mm_data_args, tokenizer=tokenizer, evaluate=True))
+            eval_datasets.append(
+                GlueDataset(mnli_mm_data_args, tokenizer=tokenizer, evaluate=True)
+            )
 
         for eval_dataset in eval_datasets:
             result = trainer.evaluate(eval_dataset=eval_dataset)
 
             output_eval_file = os.path.join(
-                training_args.output_dir, f"eval_results_{eval_dataset.args.task_name}.txt"
+                training_args.output_dir,
+                f"eval_results_{eval_dataset.args.task_name}.txt",
             )
             with open(output_eval_file, "w") as writer:
-                logger.info("***** Eval results {} *****".format(eval_dataset.args.task_name))
+                logger.info(
+                    "***** Eval results {} *****".format(eval_dataset.args.task_name)
+                )
                 for key, value in result.items():
                     logger.info("  %s = %s", key, value)
                     writer.write("%s = %s\n" % (key, value))
